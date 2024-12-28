12ccb730702c340847a4f8946eca538f52909cbf
cooltexture <cooltexture@bits-mampfer.eu>
2024-12-28 01:09:05 +0100

    added a few null checks, to prevent errors in console my code editor also reformated some stuff, i hope that is fine

--- src/main/java/gg/crystalized/essentials/CustomBows.java
+++ src/main/java/gg/crystalized/essentials/CustomBows.java
@@ -3,10 +3,8 @@ package gg.crystalized.essentials;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
@@ -15,7 +13,6 @@ import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.util.HashMap;
@@ -24,75 +21,79 @@ import java.util.logging.Level;
 
 public class CustomBows implements Listener {
 
-    public HashMap<Projectile, ArrowData> arrows = new HashMap<>();
-    @EventHandler
-    public void onBowShot(EntityShootBowEvent event){
-        if(event.isCancelled()){
-            return;
-        }
-        LivingEntity e = event.getEntity();
-        ItemStack stack = event.getBow();
+	public HashMap<Projectile, ArrowData> arrows = new HashMap<>();
+	@EventHandler
+	public void onBowShot(EntityShootBowEvent event){
+		if(event.isCancelled()){
+			return;
+		}
+		LivingEntity e = event.getEntity();
+		ItemStack stack = event.getBow();
 
-        if(stack == null){
-            return;
-        }
+		if(stack == null){
+			return;
+		}
 
-        ItemMeta meta = stack.getItemMeta();
+		ItemMeta meta = stack.getItemMeta();
 
-        if(meta == null){
-            return;
-        }
+		if(meta == null || !meta.hasCustomModelData()){
+			return;
+		}
 
-        int model = meta.getCustomModelData();
-        ArrowData.bowType type = null;
-        //note: model data of silver bow
-        if(stack.getType() == Material.BOW && model == 1){
-            type = ArrowData.bowType.marksman;
-        }else if(stack.getType() == Material.BOW && model == 3){
-            type = ArrowData.bowType.ricochet;
-        }
+		int model = meta.getCustomModelData();
+		ArrowData.bowType type = null;
+		//note: model data of silver bow
+		if(stack.getType() == Material.BOW && model == 1){
+			type = ArrowData.bowType.marksman;
+		}else if(stack.getType() == Material.BOW && model == 3){
+			type = ArrowData.bowType.ricochet;
+		}
 
-        if(type == null){
-            return;
-        }
+		if(type == null){
+			return;
+		}
 
-        ArrowData ard = new ArrowData(e, event.getForce(), event.getHand(), type,  event.getProjectile().getVelocity());
-        arrows.put((Projectile) event.getProjectile(), ard);
-        
-    }
+		ArrowData ard = new ArrowData(e, event.getForce(), event.getHand(), type,  event.getProjectile().getVelocity());
+		arrows.put((Projectile) event.getProjectile(), ard);
 
-    @EventHandler
-    public void onArrowHit(ProjectileHitEvent event){
-        Projectile pro = event.getEntity();
+	}
 
-        if(!(pro instanceof Arrow)){
-            return;
-        }
+	@EventHandler
+	public void onArrowHit(ProjectileHitEvent event){
+		Projectile pro = event.getEntity();
 
-        Arrow ar = (Arrow) pro;
-        ArrowData data = arrows.get(pro);
+		if(!(pro instanceof Arrow)){
+			return;
+		}
 
-        if(data.type == ArrowData.bowType.marksman){
-            LivingEntity e = (LivingEntity) event.getHitEntity();
-            if(e == null){
-                return;
-            }
-            Location shooterLoc = data.shooter.getLocation();
-            Location hitLoc = e.getLocation();
-            double distance = Math.floor(shooterLoc.distance(hitLoc)/10);
-            data.shooter.sendMessage("damage: "+ distance*0.5);
-            e.damage(distance * 0.5, pro);
-        }else if(data.type == ArrowData.bowType.ricochet){
-            BlockFace b = event.getHitBlockFace();
-            if(b == null){
-                Bukkit.getServer().getLogger().log(Level.SEVERE, "blockface is null returning...");
-                return;
-            }
-            Vector v = ar.getLocation().getDirection();
-            Vector vb = b.getDirection();
-            float angel = v.angle(vb);
-            data.shooter.sendMessage(v.toString());
-            data.shooter.sendMessage("angle: " + angel);
-        }
-    }
+		Arrow ar = (Arrow) pro;
+		ArrowData data = arrows.get(pro);
+
+		if (data == null) {
+			return;
+		}
+
+		if(data.type == ArrowData.bowType.marksman){
+			LivingEntity e = (LivingEntity) event.getHitEntity();
+			if(e == null){
+				return;
+			}
+			Location shooterLoc = data.shooter.getLocation();
+			Location hitLoc = e.getLocation();
+			double distance = Math.floor(shooterLoc.distance(hitLoc)/10);
+			data.shooter.sendMessage("damage: "+ distance*0.5);
+			e.damage(distance * 0.5, pro);
+		}else if(data.type == ArrowData.bowType.ricochet){
+			BlockFace b = event.getHitBlockFace();
+			if(b == null){
+				Bukkit.getServer().getLogger().log(Level.SEVERE, "blockface is null returning...");
+				return;
+			}
+			Vector v = ar.getLocation().getDirection();
+			Vector vb = b.getDirection();
+			float angel = v.angle(vb);
+			data.shooter.sendMessage(v.toString());
+			data.shooter.sendMessage("angle: " + angel);
+		}
+	}
 }
--- src/main/java/gg/crystalized/essentials/CustomCoalBasedItems.java
+++ src/main/java/gg/crystalized/essentials/CustomCoalBasedItems.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.*;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.Event.Result;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.util.Vector;
@@ -15,122 +15,148 @@ import org.bukkit.util.Vector;
 
 public class CustomCoalBasedItems implements Listener {
 
-    @EventHandler
-    public void onRightClick(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
-        if (event.getHand() != EquipmentSlot.HAND) return;
-        if (event.getAction().isRightClick()) {
-            if (player.hasCooldown(Material.COAL)) {
-                player.sendMessage(Component.text("[!] ᴛʜɪꜱ ɪᴛᴇᴍ ɪꜱ ᴏɴ ᴄᴏᴏʟᴅᴏᴡɴ! ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ").color(NamedTextColor.RED)
-                        .append(Component.text(" " + player.getCooldown(Material.COAL) / 20.0).color(NamedTextColor.WHITE))
-                        .append(Component.text(" ꜱᴇᴄᴏɴᴅꜱ ʙᴇꜰᴏʀᴇ ᴜꜱɪɴɢ ᴛʜɪꜱ ɪᴛᴇᴍ ᴀɢᴀɪɴ!").color(NamedTextColor.RED)));
-            } else {
-                if (player.getInventory().getItemInMainHand().getType().equals(Material.COAL) && player.getEquipment().getItemInMainHand().getItemMeta().hasCustomModelData()) {
-                    // Boost Orb
-                    // TODO make particles for when you launch
-                    if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 1) {
-                        player.playSound(player, "minecraft:item.armor.equip_elytra", 50, 1); //TODO either make a sound(s) for the boost orb or figure out what exact sound(s) TubNet used.
-                        player.setVelocity(new Vector(player.getVelocity().getX(), player.getVelocity().getY(), player.getVelocity().getZ()));
-                        player.setVelocity(player.getLocation().getDirection().multiply(2));
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 40);
+	@EventHandler
+	public void onRightClick(PlayerInteractEvent event) {
+		if (event.useItemInHand() == Result.DENY) {
+			return;
+		}
+		Player player = event.getPlayer();
+		if (event.getHand() != EquipmentSlot.HAND)
+			return;
+		if (event.getAction().isRightClick()) {
+			if (player.hasCooldown(Material.COAL)) {
+				player.sendMessage(Component.text("[!] ᴛʜɪꜱ ɪᴛᴇᴍ ɪꜱ ᴏɴ ᴄᴏᴏʟᴅᴏᴡɴ! ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ").color(NamedTextColor.RED)
+						.append(Component.text(" " + player.getCooldown(Material.COAL) / 20.0).color(NamedTextColor.WHITE))
+						.append(Component.text(" ꜱᴇᴄᴏɴᴅꜱ ʙᴇꜰᴏʀᴇ ᴜꜱɪɴɢ ᴛʜɪꜱ ɪᴛᴇᴍ ᴀɢᴀɪɴ!").color(NamedTextColor.RED)));
+			} else {
+				if (player.getInventory().getItemInMainHand().getType().equals(Material.COAL)
+						&& player.getEquipment().getItemInMainHand().getItemMeta().hasCustomModelData()) {
+					// Boost Orb
+					// TODO make particles for when you launch
+					if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 1) {
+						player.playSound(player, "minecraft:item.armor.equip_elytra", 50, 1); // TODO either make a sound(s) for the
+																																									// boost orb or figure out what exact
+																																									// sound(s) TubNet used.
+						player.setVelocity(
+								new Vector(player.getVelocity().getX(), player.getVelocity().getY(), player.getVelocity().getZ()));
+						player.setVelocity(player.getLocation().getDirection().multiply(2));
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 40);
 
-                        // Bridge Orb
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 2) {
-                        player.sendMessage(Component.text("Bridge orb isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
+						// Bridge Orb
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 2) {
+						player.sendMessage(Component.text("Bridge orb isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
 
-                        // Explosive Orb
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 3) {
-                        Vector direction = player.getEyeLocation().getDirection();
-                        Fireball fireball = player.launchProjectile(Fireball.class, direction);
-                        fireball.getLocation().add(fireball.getVelocity().normalize().multiply(3));
-                        fireball.setYield(3);
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
+						// Explosive Orb
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 3) {
+						Vector direction = player.getEyeLocation().getDirection();
+						Fireball fireball = player.launchProjectile(Fireball.class, direction);
+						fireball.getLocation().add(fireball.getVelocity().normalize().multiply(3));
+						fireball.setYield(3);
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
 
-                        // Grappling Orb
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 4) {
-                        player.sendMessage(Component.text("Grappling orb isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
-                        /*
-                        Vector direction = player.getEyeLocation().getDirection();
-                        Snowball snowball = player.launchProjectile(Snowball.class, direction);
-                        snowball.getLocation().add(snowball.getVelocity().normalize().multiply(3));
-                         */
+						// Grappling Orb
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 4) {
+						player.sendMessage(Component.text("Grappling orb isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
+						/*
+						 * Vector direction = player.getEyeLocation().getDirection();
+						 * Snowball snowball = player.launchProjectile(Snowball.class, direction);
+						 * snowball.getLocation().add(snowball.getVelocity().normalize().multiply(3));
+						 */
 
-                        // Health Orb
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 5) {
-                        player.sendMessage(Component.text("Health orb isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
-                        // It's not well known what Health Orbs did since they weren't in TubNet for very long
+						// Health Orb
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 5) {
+						player.sendMessage(Component.text("Health orb isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
+						// It's not well known what Health Orbs did since they weren't in TubNet for
+						// very long
 
-                        // Knockout Orb
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 6) {
-                        player.sendMessage(Component.text("Knockout orb isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
+						// Knockout Orb
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 6) {
+						player.sendMessage(Component.text("Knockout orb isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
 
-                        // Poison Orb
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 7) {
-                        player.sendMessage(Component.text("Poison orb isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
-                        //It's not well known what Poison Orbs did since they weren't in TubNet for very long
+						// Poison Orb
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 7) {
+						player.sendMessage(Component.text("Poison orb isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
+						// It's not well known what Poison Orbs did since they weren't in TubNet for
+						// very long
 
-                        // Winged Orb
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 8) {
-                        player.sendMessage(Component.text("Winged orb isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
+						// Winged Orb
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 8) {
+						player.sendMessage(Component.text("Winged orb isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
 
-                        // Antiair Totem
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 9) {
-                        player.sendMessage(Component.text("Antiair Totem isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
+						// Antiair Totem
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 9) {
+						player.sendMessage(Component.text("Antiair Totem isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
 
-                        // Cloud Totem
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 10) {
-                        player.sendMessage(Component.text("Cloud Totem isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
-                    /*
-                    Cloud Totems acted differently in different games
-                    In Crystal Rush, the platform was made from glass (tinted glass?)
-                    In Knockout the platform was made out of Crystal Blocks that matched your team's/leather armor's colour
-                    We will need to somehow account for that by detecting what game is being played.
-                    */
+						// Cloud Totem
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 10) {
+						player.sendMessage(Component.text("Cloud Totem isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
+						/*
+						 * Cloud Totems acted differently in different games
+						 * In Crystal Rush, the platform was made from glass (tinted glass?)
+						 * In Knockout the platform was made out of Crystal Blocks that matched your
+						 * team's/leather armor's colour
+						 * We will need to somehow account for that by detecting what game is being
+						 * played.
+						 */
 
-                        // Defense Totem
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 11) {
-                        player.sendMessage(Component.text("Defence isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
+						// Defense Totem
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 11) {
+						player.sendMessage(Component.text("Defence isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
 
-                        // Healing Totem
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 12) {
-                        player.sendMessage(Component.text("Healing Totem isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
+						// Healing Totem
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 12) {
+						player.sendMessage(Component.text("Healing Totem isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
 
-                        // Launch Totem
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 13) {
-                        player.sendMessage(Component.text("Launch Totem isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
+						// Launch Totem
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 13) {
+						player.sendMessage(Component.text("Launch Totem isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
 
-                        // Slime Totem
-                    } else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 14) {
-                        player.sendMessage(Component.text("Slime Totem isn't currently implemented yet")); //TODO
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-                        player.setCooldown(Material.COAL, 5);
-                    }
-                }
-            }
-        }
-    }
+						// Slime Totem
+					} else if (player.getEquipment().getItemInMainHand().getItemMeta().getCustomModelData() == 14) {
+						player.sendMessage(Component.text("Slime Totem isn't currently implemented yet")); // TODO
+						player.getInventory().getItemInMainHand()
+								.setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+						player.setCooldown(Material.COAL, 5);
+					}
+				}
+			}
+		}
+	}
 }
