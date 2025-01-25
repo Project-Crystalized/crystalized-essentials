19038d57c98f5ee47af03ba2cb281343cf6d8cea
ladycattv <ladyccattv@gmail.com>
2025-01-25 22:05:50 +0100

    started Explo arrow code (coded cardinal features first try I'm so happy)

--- src/main/java/gg/crystalized/essentials/CustomArrows.java
+++ src/main/java/gg/crystalized/essentials/CustomArrows.java
@@ -1,6 +1,8 @@
 package gg.crystalized.essentials;
 
 import com.destroystokyo.paper.ParticleBuilder;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Location;
@@ -19,12 +21,16 @@ import org.bukkit.potion.PotionType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.BoundingBox;
+import org.bukkit.util.Vector;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.logging.Level;
 
 import static org.bukkit.Color.PURPLE;
 import static org.bukkit.Particle.DUST;
+import static org.bukkit.Sound.BLOCK_NOTE_BLOCK_IMITATE_CREEPER;
+import static org.bukkit.Sound.ENTITY_GENERIC_EXPLODE;
 import static org.bukkit.damage.DamageType.ARROW;
 import static org.bukkit.entity.EntityType.AREA_EFFECT_CLOUD;
 import static org.bukkit.entity.EntityType.SPECTRAL_ARROW;
@@ -90,6 +96,66 @@ public class CustomArrows implements Listener {
                 }
             }.runTaskTimer(crystalized_essentials.getInstance(),1,1);
 
+        }else if(meta.getCustomModelData() == 2){
+            Collection<LivingEntity> nearby = loc.getNearbyLivingEntities(5);
+            Collection<LivingEntity> notSoNearby = loc.getNearbyLivingEntities(10);
+            for(LivingEntity e : notSoNearby){
+                if(nearby.contains(e)){
+                    notSoNearby.remove(e);
+                }
+            }
+
+
+            if(event.getHitEntity() != null){
+                for(LivingEntity e : nearby){
+                    Location eLoc = e.getLocation();
+                    Vector v = new Vector(eLoc.getX() - loc.getX(),eLoc.getY() - loc.getY(), eLoc.getZ() - loc.getZ());
+                    v = v.normalize().multiply(5);
+                    e.damage(3);
+                    e.setVelocity(v);
+                }
+
+                for(LivingEntity e : notSoNearby){
+                    Location eLoc = e.getLocation();
+                    Vector v = new Vector(eLoc.getX() - loc.getX(),eLoc.getY() - loc.getY(), eLoc.getZ() - loc.getZ());
+                    v = v.normalize().multiply(2);
+                    e.damage(1.5);
+                    e.setVelocity(v);
+                }
+                return;
+            }
+
+            new BukkitRunnable(){
+                int i = 0;
+                public void run(){
+                    if(i >= 3){
+                        cancel();
+                    }
+                    Bukkit.getServer().getWorld("world").playSound(event.getEntity(), BLOCK_NOTE_BLOCK_IMITATE_CREEPER, 1, 1);
+                    i++;
+                }
+            }.runTaskTimer(crystalized_essentials.getInstance(), 0, 20);
+
+            new BukkitRunnable(){
+                public void run(){
+                    for(LivingEntity e : nearby){
+                        Location eLoc = e.getLocation();
+                        Vector v = new Vector(eLoc.getX() - loc.getX(),eLoc.getY() - loc.getY(), eLoc.getZ() - loc.getZ());
+                        v = v.normalize().multiply(5);
+                        e.damage(3);
+                        e.setVelocity(v);
+                    }
+
+                    for(LivingEntity e : notSoNearby){
+                        Location eLoc = e.getLocation();
+                        Vector v = new Vector(eLoc.getX() - loc.getX(),eLoc.getY() - loc.getY(), eLoc.getZ() - loc.getZ());
+                        v = v.normalize().multiply(2);
+                        e.damage(1.5);
+                        e.setVelocity(v);
+                    }
+                    Bukkit.getServer().getWorld("world").playSound(event.getEntity(), ENTITY_GENERIC_EXPLODE, 1, 1);
+                }
+            }.runTaskLater(crystalized_essentials.getInstance(), 3*20);
         }
         
     }
